e5c122c1a64bcefce0cbcf282ed81353a90f7183
Ludger Möhlenkamp <ludger.moehlenkamp@googlemail.com>
2020-02-06 21:19:43 +0100

    + singleton.h   - fixed declaration of copy assignment operator

--- singleton.h
+++ singleton.h
@@ -5,7 +5,7 @@ private:
 
   singleton() {};
   singleton(const singleton&) = default;
-  singleton& singleton::operator=(const singleton&) = default;
+  singleton& operator=(const singleton&) = default;
 public:
   static singleton* getInstance();
   ~singleton();
