d86e74babf43a086240003b0f38b300b991968cf
Ludger Möhlenkamp <ludger.moehlenkamp@googlemail.com>
2020-10-14 21:32:56 +0200

    added singleton unit test

---
+++ gitTestTest.cpp
@@ -0,0 +1,14 @@
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE singleton_test_module
+
+#include <boost/test/unit_test.hpp>
+#include "singleton.h"
+
+BOOST_AUTO_TEST_CASE(instanciate_singleton)
+{
+  singleton* firstInst = singleton::getInstance();
+  singleton* secondInst = singleton::getInstance();
+
+  BOOST_CHECK( firstInst != nullptr);
+  BOOST_CHECK( secondInst == nullptr);
+}
