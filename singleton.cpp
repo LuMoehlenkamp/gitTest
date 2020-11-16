aec680cf7a42b584d5f2a83e3db74c4158b1f8b4
Ludger Möhlenkamp <ludger.moehlenkamp@googlemail.com>
2020-11-16 22:01:00 +0100

    POW-4 added singleton pattern implementation

--- singleton.cpp
+++ singleton.cpp
@@ -1,17 +1,10 @@
-#include <iostream>
-
 #include "singleton.h"
 
 singleton::~singleton() {};
 
 singleton* singleton::getInstance() {
   if ( singletonInst == nullptr) {
-    std::cout<< "creating only possible instance now" << std::endl;
     singletonInst = new singleton();
-  }
-  else {
-    std::cout<< "single instance already created" << std::endl;
-  }
-  
+  }  
   return singletonInst;
 }
